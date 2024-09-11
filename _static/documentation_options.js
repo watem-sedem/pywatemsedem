15b12d14c1725e9a9ffb53007738e2fb44bfd108
johanvdw <johanvdw@users.noreply.github.com>
2024-09-11 07:00:04 +0000

    deploy: 7dbcab98aa9bf668313e7c4d9b08f41045577b49

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '0.0.1a8.post1.dev2+g21e0c23',
+    VERSION: '0.0.1a8.post1.dev3+g7dbcab9',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
