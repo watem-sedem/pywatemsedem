870f02e2f4a81907022224ad48f738ce06b43658
SachaGobeyn <sacha@fluves.com>
2024-09-23 15:49:28 +0200

    bug: in some cases, there are no parcels in the case study, in this case, one should allow a nodata array

--- src/pywatemsedem/scenario.py
+++ src/pywatemsedem/scenario.py
@@ -524,7 +524,7 @@ class Scenario:
             arr = self._vct_parcels.rasterize(
                 self.catchm.mask_raster, self.rp.epsg, col="NR", gdal=True
             )
-            return self.raster_factory(arr)
+            return self.raster_factory(arr, allow_nodata_array=True)
         else:
             return None
 
