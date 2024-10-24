5a3897ff2c4915172babcf277f6ceb8909c8f670
Daan Renders <daan@fluves.com>
2024-10-24 15:34:00 +0200

    Remove decorator

--- tests/test_choices.py
+++ tests/test_choices.py
@@ -151,7 +151,6 @@ def test_initialisation_ws_extension_parameters_estimate_clay_content():
     assert ws_ext_param.clay_content_parent_material.mandatory
 
 
-@pytest.mark.skip(reason="not yet implemented")
 def test_initialisation_ws_output():
     """Test if the WSOutput object is initialised ok"""
     ws_out = WSOutput()
