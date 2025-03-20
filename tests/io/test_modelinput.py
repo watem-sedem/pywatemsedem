ebb998ccfbe8defda577177b57a4a5418410bc31
Baris Oztas <baris@fluves.com>
2025-03-20 11:16:13 +0100

    do not skip failing tests

--- tests/io/test_modelinput.py
+++ tests/io/test_modelinput.py
@@ -2,17 +2,11 @@
 
 from pathlib import Path
 
-import pytest
-
 from pywatemsedem.io.modelinput import Modelinput
 
 # testing normal functioning
 
 
-@pytest.mark.skip(
-    reason="test gives saga-issues in conda pipeline, ommited, raised as"
-    " issue in saga"
-)
 def test_modelinput_all():
     """tests for Modelinput class"""
     # folder = Path(r"pywatemsedem/tests/io/data")
