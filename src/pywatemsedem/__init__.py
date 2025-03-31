f568ac330c430e19057615130e56a23c7935917e
Baris Oztas <baris@fluves.com>
2025-03-31 15:18:39 +0200

    do not check for older python version as it is not supported anymore (#47)

--- src/pywatemsedem/__init__.py
+++ src/pywatemsedem/__init__.py
@@ -1,12 +1,4 @@
-import sys
-
-if sys.version_info[:2] >= (3, 8):
-    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
-    from importlib.metadata import PackageNotFoundError  # pragma: no cover
-    from importlib.metadata import version
-else:
-    from importlib_metadata import PackageNotFoundError  # pragma: no cover
-    from importlib_metadata import version
+from importlib.metadata import PackageNotFoundError, version
 
 try:
     # Change here if project is renamed and does not equal the package name
