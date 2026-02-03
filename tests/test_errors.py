d6bf5db8732b23904f649bbffe0bdd90a1e9b625
Baris Oztas <baris@fluves.com>
2026-02-03 14:20:03 +0000

    add test cases that causes failure for error handling functions

--- tests/test_errors.py
+++ tests/test_errors.py
@@ -1,99 +1,112 @@
 import numpy as np
+import pandas as pd
 import pytest
 
 from pywatemsedem.errors import (
     PywatemsedemRasterValueError,
+    PywatemsedemVectorAttributeError,
+    PywatemsedemVectorAttributeValueError,
+    attribute_continuous_value_error,
+    attribute_discrete_value_error,
+    missing_attribute_error_in_vct,
     raster_discrete_value_error,
 )
 
 
-@pytest.mark.parametrize(
-    "raster_array, allowed_values, should_raise",
-    [
-        (np.array([1, 2]), [1, 2], False),
-        (np.array([1, 1, 1, 1]), [1, 2], False),
-        (np.array([1, 2, 2, 3, 2, 1]), [1, 2, 3], False),
-        (np.array([1, 2, 4, 2, 1]), [1, 2, 3], True),
-        (np.array([1, 2, 4, 2, 1]), [0], True),
-    ],
-    ids=[
-        "valid_case_perfect_match",
-        "valid_case_does_not_contain_all_values",
-        "valid_case_combintaion",
-        "invalid_case_single_value",
-        "invalid_case_all_values",
-    ],
-)
-def test_raster_discrete_value_error_handling_without_classes(
-    raster_array, allowed_values, should_raise
-):
-    """Verify raster_discrete_value_error for both valid and invalid cases.
+def test_raster_discrete_value_error_allowed():
+    """Verify that no error is raised for allowed discrete values in raster."""
+    arr = np.array([1, 2, 2, 1])
+    allowed = [1, 2]
+    raster_discrete_value_error(arr, tag="test", allowed_values=allowed)
 
-    Special case where the classes are not considered.
 
-    Parameters
-    ----------
-    raster_array: numpy.ndarray
-        Input raster array that needs to be checked.
-    allowed_values: list
-        List of allowed values for the raster.
-    should_raise: bool
-        Indicates whether the function should raise an error or not.
-    """
-
-    tag = "test_raster_to_be_checked"
-
-    if should_raise:
-        with pytest.raises(PywatemsedemRasterValueError):
-            raster_discrete_value_error(raster_array, tag, allowed_values)
-    else:
-        # Should not raise an error
-        raster_discrete_value_error(raster_array, tag, allowed_values)
+def test_raster_discrete_value_error_not_allowed():
+    """Verify that error is raised for not allowed discrete values in raster."""
+    arr = np.array([1, 2, 3])
+    allowed = [1, 2]
+    with pytest.raises(PywatemsedemRasterValueError):
+        raster_discrete_value_error(arr, tag="test", allowed_values=allowed)
 
 
 @pytest.mark.parametrize(
-    "raster_array, allowed_values, classes, should_raise",
+    "df,lower,upper",
     [
-        (np.array([1, 2]), [1, 2], ["one", "two"], False),
-        (np.array([1, 1, 1, 1]), [1, 2], ["one", "two"], False),
-        (np.array([1, 2, 2, 3, 2, 1]), [1, 2, 3], ["one", "two", "three"], False),
-        (np.array([1, 2, 4, 2, 1]), [1, 2, 3], ["one", "two", "three"], True),
-        (np.array([1, 2, 4, 2, 1]), [0], ["zero"], True),
-    ],
-    ids=[
-        "valid_case_perfect_match",
-        "valid_case_does_not_contain_all_values",
-        "valid_case_combiantion",
-        "invalid_case_single_value",
-        "invalid_case_all_values",
+        (pd.DataFrame({"val": [1, 2, 3]}), 1, 3),
+        (pd.DataFrame({"val": [0, 1, 2]}), 0, 2),
+        (pd.DataFrame({"val": [5, 6, 7]}), 5, 7),
     ],
 )
-def test_raster_discrete_value_error_handling_with_classes(
-    raster_array, allowed_values, classes, should_raise
-):
-    """Verify raster_discrete_value_error for both valid and invalid cases.
+def test_attribute_continuous_value_error_allowed(df, lower, upper):
+    attribute_continuous_value_error(
+        df, tag="test", attribute="val", lower=lower, upper=upper
+    )
 
-    Special case where the classes are provided.
 
-    Parameters
-    ----------
-    raster_array: numpy.ndarray
-        Input raster array that needs to be checked.
-    allowed_values: list
-        List of allowed values for the raster.
-    classes: list
-        List of class names corresponding to the allowed values.
-    should_raise: bool
-        Indicates whether the function should raise an error or not.
-    """
+@pytest.mark.parametrize(
+    "df,lower,upper",
+    [
+        (pd.DataFrame({"val": [1, 2, 5]}), 1, 3),
+        (pd.DataFrame({"val": [-1, 0, 1]}), 0, 2),
+        (pd.DataFrame({"val": [5, 6, 8]}), 5, 7),
+    ],
+)
+def test_attribute_continuous_value_error_not_allowed(df, lower, upper):
+    with pytest.raises(PywatemsedemVectorAttributeValueError):
+        attribute_continuous_value_error(
+            df, tag="test", attribute="val", lower=lower, upper=upper
+        )
 
-    tag = "test_raster_to_be_checked"
 
-    if should_raise:
-        with pytest.raises(PywatemsedemRasterValueError):
-            raster_discrete_value_error(
-                raster_array, tag, allowed_values, classes=classes
-            )
-    else:
-        # Should not raise an error
-        raster_discrete_value_error(raster_array, tag, allowed_values, classes=classes)
+@pytest.mark.parametrize(
+    "df,allowed",
+    [
+        (pd.DataFrame({"cat": [1, 2, 1]}), {1, 2}),
+        (pd.DataFrame({"cat": [0, 0, 0]}), {0}),
+        (pd.DataFrame({"cat": [5, 6, 5]}), {5, 6}),
+    ],
+)
+def test_attribute_discrete_value_error_allowed(df, allowed):
+    attribute_discrete_value_error(
+        df, tag="test", attribute="cat", allowed_values=allowed
+    )
+
+
+@pytest.mark.parametrize(
+    "df,allowed",
+    [
+        (pd.DataFrame({"cat": [1, 2, 3]}), {1, 2}),
+        (pd.DataFrame({"cat": [0, 1, 2]}), {0}),
+        (pd.DataFrame({"cat": [5, 6, 7]}), {5, 6}),
+    ],
+)
+def test_attribute_discrete_value_error_not_allowed(df, allowed):
+    with pytest.raises(PywatemsedemVectorAttributeValueError):
+        attribute_discrete_value_error(
+            df, tag="test", attribute="cat", allowed_values=allowed
+        )
+
+
+@pytest.mark.parametrize(
+    "df,req",
+    [
+        (pd.DataFrame({"a": [1], "b": [2]}), {"a", "b"}),
+        (pd.DataFrame({"x": [1], "y": [2], "z": [3]}), {"x", "y"}),
+    ],
+)
+def test_missing_attribute_error_in_vct_allowed(df, req):
+    missing_attribute_error_in_vct(df, tag="test", req_attributes=req)
+
+
+# Error: missing required attribute
+
+
+@pytest.mark.parametrize(
+    "df,req",
+    [
+        (pd.DataFrame({"a": [1]}), {"a", "b"}),
+        (pd.DataFrame({"x": [1], "z": [2]}), {"x", "y"}),
+    ],
+)
+def test_missing_attribute_error_in_vct_not_allowed(df, req):
+    with pytest.raises(PywatemsedemVectorAttributeError):
+        missing_attribute_error_in_vct(df, tag="test", req_attributes=req)
