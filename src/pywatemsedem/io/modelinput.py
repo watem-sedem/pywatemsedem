c13bbdb6928bba42159255b1e70e8047b7644833
Daan Renders <daan@fluves.com>
2026-03-25 13:14:07 +0100

    Add all possible model input files to modelinput class

--- src/pywatemsedem/io/modelinput.py
+++ src/pywatemsedem/io/modelinput.py
@@ -8,6 +8,7 @@ import pandas as pd
 from matplotlib import colors
 
 from pywatemsedem.geo.factory import Factory
+from pywatemsedem.geo.rasters import AbstractRaster
 from pywatemsedem.geo.utils import (
     check_raster_properties_raster_with_template,
     mask_array_with_val,
@@ -68,20 +69,27 @@ class Modelinput(Factory):
         self.mask = template
 
         # DATA
-        self._cfactor = None  # attribute
-        self._buffers = None
-        self._dtm = None
-        self._kfactor = None
-        self._ktc = None
-        self._outlet = None
-        self._pfactor = None
-        self._compositelanduse = None
-        self._ptef = None
-        self._riversegments = None
-        self._riverrouting = None
-        self._sewers = None
+        self._dtm = AbstractRaster()
+        self._compositelanduse = AbstractRaster()
+        self._pfactor = AbstractRaster()
+        self._kfactor = AbstractRaster()
+        self._cfactor = AbstractRaster()  # attribute
+        self._ktc = AbstractRaster()
+        self._outlet = AbstractRaster()
+        self._ktil = AbstractRaster()
+        self._sewers = AbstractRaster()
+        self._tillagedirection = AbstractRaster()
+        self._orientedroughness = AbstractRaster()
+        self._buffers = AbstractRaster()
+        self._ditches = AbstractRaster()
+        self._dams = AbstractRaster()
+        self._riversegments = AbstractRaster()
         self._upstreamsegments = None
         self._adjacentsegments = None
+        self._riverrouting = AbstractRaster()
+        self._cn = AbstractRaster()
+        self._rainfall = None
+        self._ptef = AbstractRaster()
 
     @property
     def cfactor(self):
@@ -791,3 +799,47 @@ class Modelinput(Factory):
         valid_non_nan(array)
         valid_values(array, unique_values=np.unique(self.riversegments.arr).tolist())
         valid_array_type(array, required_type=np.int64)
+
+    @property
+    def ktil(self):
+        """Getter ktil map."""
+        return self._ktil
+
+    @ktil.setter
+    def ktil(self, raster):
+        """Setter for ktil map"""
+        self._ktil = self.raster_factory(raster, flag_mask=False)
+        # checks
+        valid_non_nan(self._ktil.arr)
+        valid_array_type(self._ktil.arr, required_type=np.int16)
+        check_raster_properties_raster_with_template(self.rp, raster, epsg=self.rp.epsg)
+
+    @property
+    def tillagedirection(self):
+        """Getter tillagedirection map."""
+        return self._tillagedirection
+
+    @property
+    def orientedroughness(self):
+        """Getter orientedroughness map."""
+        return self._orientedroughness
+
+    @property
+    def ditches(self):
+        """Getter ditches map."""
+        return self._ditches
+
+    @property
+    def dams(self):
+        """Getter dams map."""
+        return self._dams
+
+    @property
+    def cn(self):
+        """Getter cn map."""
+        return self._cn
+
+    @property
+    def rainfall(self):
+        """Getter rainfall map."""
+        return self._rainfall
