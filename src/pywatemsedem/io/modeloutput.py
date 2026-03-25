1a31b4b1f019179ee91e98f188fbaeb166be5bf3
Daan Renders <daan@fluves.com>
2026-03-25 13:30:39 +0100

    Fix imports in modeloutput.py

--- src/pywatemsedem/io/modeloutput.py
+++ src/pywatemsedem/io/modeloutput.py
@@ -11,8 +11,6 @@ import pandas as pd
 from matplotlib import colors
 from shapely.geometry import LineString
 
-from pywatemsedem.plots import plot_cumulative_sedimentload
-
 from ..geo.factory import Factory
 from ..geo.utils import (
     check_raster_properties_raster_with_template,
@@ -28,8 +26,13 @@ from ..geo.utils import (
     rst_to_vct_points,
     write_arr_as_rst,
 )
-from ..io.plots import axes_creator  # hvplot_continuous_raster,
-from ..io.plots import log_scale_enabler, plot_continuous_raster, plot_output_raster
+from ..io.plots import (
+    axes_creator,
+    log_scale_enabler,
+    plot_continuous_raster,
+    plot_cumulative_sedimentload,
+    plot_output_raster,
+)
 from ..io.valid import valid_array_type, valid_boundaries, valid_non_nan
 
 IMPLEMENTED_RASTER_TYPES = [np.int16, np.int32, np.int64, np.float32, np.float64]
