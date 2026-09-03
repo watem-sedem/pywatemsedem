388409006be740b245e5656eab783346edf48aec
daarende <daarende@users.noreply.github.com>
2026-09-03 13:55:33 +0000

    deploy: aa8208729c7b0fd1a6ae2cd9c11da2946ffb22d6

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: '1.2.0.post1.dev1+g0aa8bd8e1',
+    VERSION: '1.2.0.post1.dev2+gaa8208729',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
